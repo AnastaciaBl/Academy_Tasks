97f165b91d313da5a3516877eae1171f0383d0c7
AnastaciaBl <AnastaciaBl96@gmail.com>
2018-05-01 01:59:27 +0300

    Task4 without user interface. Create a project for Task5.
    File parser was created.

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                WordChange c = new WordChange("text.txt", "text", "newText");
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task4/WordChange.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace Task4
+{
+    class WordChange:FileParser
+    {
+        public string OldWord { get; private set; }
+        public string NewWord { get; private set; }
+
+        public WordChange(string path, string oldWord, string newWord):base(path)
+        {
+            OldWord = oldWord;
+            NewWord = newWord;
+            changeText();
+        }
+
+        private void changeText()
+        {
+            Text = Text.Replace(OldWord, NewWord);
+            using (StreamWriter sw = new StreamWriter(Path))
+            {
+                sw.WriteLine(Text);
+            }
+        }
+    }
+}
