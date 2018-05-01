9b1ed565b85e647e35dcafb4ff298ffa7dbabac2
AnastaciaBl <AnastaciaBl96@gmail.com>
2018-05-01 19:35:26 +0300

    Task3 without user interface.
    Triangle`s sort.

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle[] triangles = new Triangle[]
+            {
+                new Triangle("New1", 8,7,6),
+                new Triangle("New2", 3,5,4),
+                new Triangle("New3", 2,2,3),
+            };
+            Array.Sort(triangles);
+            foreach (Triangle t in triangles)
+                Console.WriteLine(t.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task3/Triangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task3
+{
+    class Triangle:IComparable<Triangle>
+    {
+        public string Name { get; private set; }
+        public double A { get; private set; }
+        public double B { get; private set; }
+        public double C { get; private set; }
+        public double Area { get; private set; }
+
+        public Triangle(string name, double a, double b, double c)
+        {
+            Name = name;
+            A = a;
+            B = b;
+            C = c;
+            Area = findAreaOfTriangle();
+        }
+
+        private double findAreaOfTriangle()
+        {
+            double p = 0.5 * (A + B + C);
+            double area = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            return Math.Round(area, 2);
+        }
+
+        public int CompareTo(Triangle other)
+        {
+            if (Area > other.Area)
+                return 1;
+            else if (Area < other.Area)
+                return -1;
+            else
+                return 0;
+        }
+
+        public override string ToString()
+        {
+            string str = string.Format("[{0}]: {1} cm", Name, Area);
+            return str;
+        }
+    }
+}
