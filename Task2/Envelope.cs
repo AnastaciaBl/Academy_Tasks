f2e96ca4fe86e38a8611b00e862321e88f8dcf6a
AnastaciaBl <AnastaciaBl96@gmail.com>
2018-05-01 20:50:05 +0300

    Task2 without user interface.
    Is one envelop fits into another

---
+++ Task2/Envelope.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Task2
+{
+    class Envelope
+    {
+        public double A { get; private set; }
+        public double B { get; private set; }
+
+        public Envelope(double a, double b)
+        {
+            if (a >= b)
+            {
+                A = a;
+                B = b;
+            }
+            else
+            {
+                A = b;
+                B = a;
+            }
+        }
+
+        public bool IsFitsIntoThisEnvelope(Envelope otherEnvelope)
+        {
+            if (isFitsIntoFirstCondition(otherEnvelope) || isFitsIntoSecondCondition(otherEnvelope))
+                return true;
+            else return false;
+        }
+
+        private bool isFitsIntoFirstCondition(Envelope other)
+        {
+            if (other.A <= A && other.B <= B)
+                return true;
+            else return false;
+        }
+
+        private bool isFitsIntoSecondCondition(Envelope other)
+        {
+            double conditionalValue = (2 * other.A * other.B * A + (other.A * other.A - other.B * other.B) *
+                Math.Sqrt(other.A * other.A + other.B * other.B - A * A)) /
+                (other.A * other.A + other.B * other.B);
+            if (other.A > A && conditionalValue <= B)
+                return true;
+            else return false;
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Envelope e1 = new Envelope(13, 88);
+            //Envelope e2 = new Envelope(81, 59);
+            Envelope e1 = new Envelope(13, 10);
+            Envelope e2 = new Envelope(5, 40);
+            Console.WriteLine(e2.IsFitsIntoThisEnvelope(e1));
+            Console.ReadLine();
+        }
+    }
+}
