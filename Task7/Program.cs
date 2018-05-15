e5dc468e6cf95e8d43a83756479e5c270c8d37f8
AnastaciaBl <AnastaciaBl96@gmail.com>
2018-05-16 00:07:22 +0300

    Task 7 was refactored.

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -4,29 +4,31 @@ namespace Task7
 {
     class Program
     {
-        private static string startInfo = "Task #7. Find all natural numbers which smaller than input number in the square.";
-        private static string wrongInputInfo = "It should be a integer number.";
-        private static string numberInput = "Input a number, please.";
-        private static string startAgain = "Do you want to start a program again? y/n";
-        private static string answer = "Answer: {0}";
+        private const string StartInfo = "Task #7. Find all natural numbers which smaller than input number in the square.";
+        private const string WrongInputInfo = "It should be a integer number.";
+        private const string NumberInput = "Input a number, please.";
+        private const string StartAgain = "Do you want to start a program again? y/n";
+        private const string Answer = "Answer: {0}";
 
         static void Main(string[] args)
         {
-            string number, endCheck;
+            string endCheck;
+            int number;
             do
             {
-                Console.WriteLine(startInfo);
+                Console.WriteLine(StartInfo);
                 while (true)
                 {
-                    Console.WriteLine(numberInput);
-                    number = Console.ReadLine();
-                    if (CheckUserInput.IsIntegerNumber(number))
+                    Console.WriteLine(NumberInput);
+                    string temp = Console.ReadLine();
+                    if (Int32.TryParse(temp, out number))
                         break;
-                    else Console.WriteLine(wrongInputInfo);
+                    else Console.WriteLine(WrongInputInfo);
                 }
-                NaturalNumbers naturalNumbers = new NaturalNumbers(Convert.ToInt32(number));
-                Console.WriteLine(string.Format(answer, naturalNumbers.ToString()));
-                Console.WriteLine(startAgain);
+                NaturalNumbers naturalNumbers = new NaturalNumbers(number);
+                //NaturalNumbers naturalNumbers = new NaturalNumbers(Int32.MaxValue);
+                Console.WriteLine(string.Format(Answer, naturalNumbers.ToString()));
+                Console.WriteLine(StartAgain);
                 endCheck = Console.ReadLine();
             } while (endCheck.ToLower() == "y");
         }
--- UnitTestProjectTask7/NaturalNumbers.cs
+++ UnitTestProjectTask7/NaturalNumbers.cs
@@ -1,23 +1,31 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 
 namespace Task7
 {
     class NaturalNumbers
     {
         public int[] Numbers { get; private set; }
-        private int checkNumber;
+        private double checkNumber;
+        private string textRepresentation;
 
         public NaturalNumbers(int _checkNumber)
         {
-            checkNumber = _checkNumber;
+            checkNumber = System.Math.Sqrt(_checkNumber);
             Numbers = createSetOfNumbers();
+            textRepresentation = createTextRepresentation();
+        }
+
+        public override string ToString()
+        {
+            return textRepresentation;
         }
 
         private int[] createSetOfNumbers()
         {
             List<int> setOfNumbers = new List<int>();
             int tempNumber = 1;
-            while(tempNumber*tempNumber<checkNumber)
+            while (tempNumber < checkNumber)
             {
                 setOfNumbers.Add(tempNumber);
                 tempNumber++;
@@ -25,17 +33,20 @@ namespace Task7
             return setOfNumbers.ToArray();
         }
 
-        public override string ToString()
+        private string createTextRepresentation()
         {
-            string str = string.Empty;
             if (Numbers.Length != 0)
             {
+                var sb = new StringBuilder();
                 for (int i = 0; i < Numbers.Length - 1; i++)
-                    str += Numbers[i] + ",";
-                str += Numbers[Numbers.Length - 1];
+                {
+                    sb.Append(Numbers[i]);
+                    sb.Append(',');
+                }
+                sb.Append(Numbers[Numbers.Length - 1]);
+                return sb.ToString();
             }
-            else str = string.Format("This set hasn`t numbers which smaller than {0} in the square.", checkNumber);
-            return str;
+            else return string.Format("This set hasn`t numbers which smaller than {0} in the square.", checkNumber);            
         }
     }
 }
